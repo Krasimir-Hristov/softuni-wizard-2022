25e3b34ca1d0c137700c981599a17e24a6db8278
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-21 22:59:59 +0200

    Create wizard with initial position on start

--- src/js/gameObjects.js
+++ src/js/gameObjects.js
@@ -2,10 +2,27 @@
 function initGameObjects() {
     const startScreen = document.querySelector('.start-screen');
     const gameScreen = document.querySelector('.game-screen');
+    
 
     return {
         startScreen,
-        gameScreen
+        gameScreen,
+        createWizard(initialState) {
+            let wizardElement = document.createElement('div');
+            wizardElement.classList.add('wizard');
+
+            wizardElement.style.width = initialState.width + 'px';
+            wizardElement.style.height = initialState.height + 'px';
+
+            wizardElement.style.left = initialState.startX + 'px';
+            wizardElement.style.top = initialState.startY + 'px';
+
+            this.wizardElement = wizardElement;
+            gameScreen.appendChild(wizardElement)
+
+            return wizardElement;
+        }
+       
     };
 }
 
