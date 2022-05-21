3f5f7eb0ed16b45c9d85162b55bc3b315d59fcd4
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-21 21:45:48 +0200

    Separate login into different files

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
-const startScreen = document.querySelector('.start-screen');
-const gameScreen = document.querySelector('.game-screen');
-startScreen.addEventListener('click', (e) => {
-    e.currentTarget.classList.add('hidden');
-    gameScreen.classList.remove('hidden')
-})
+let state = initState();
+let game = initGameObjects();
+
+
+game.startScreen.addEventListener('click', (e) => {
+    game.startScreen.classList.add('hidden');
+    game.gameScreen.classList.remove('hidden');
+});
