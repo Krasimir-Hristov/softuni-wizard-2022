77c1156b7b4a5447bf145f54589d50a646ddf419
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-22 18:59:41 +0200

    Add move to the right

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -5,6 +5,16 @@ function start(state, game) {
 }
 
 function gameLoop(state, game) {
+    const { wizard } = state;
+    const {wizardElement} = game;
+
+        // move wizard
+    if(state.keys.KeyD) {
+        wizard.posX += 5;
+    }
+
+    // Render
+    wizardElement.style.left = wizard.posX + 'px';
 
     window.requestAnimationFrame(gameLoop.bind(null, state, game));
 }
