fcd877bfc5f0b1edb42db23acb4b0dc0461df47d
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-22 21:13:43 +0200

    Add collision to fireball and bugs

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -17,6 +17,8 @@ function gameLoop(state, game, timestamp) {
         game.wizardElement.style.backgroundImage = 'url("/src/images/wizard.png")';
     }
 
+    
+
     //Spawn bugs
     if (timestamp > state.bugStats.nextSpawnTimestamp) {
         game.createBug(state.bugStats);
@@ -27,7 +29,8 @@ function gameLoop(state, game, timestamp) {
 
 
     // Render bugs
-    document.querySelectorAll('.bug').forEach(bug => {
+    let bugElements =document.querySelectorAll('.bug');
+    bugElements.forEach(bug => {
         let posX = parseInt(bug.style.left);
 
         if (posX > 0) {
@@ -41,8 +44,16 @@ function gameLoop(state, game, timestamp) {
     document.querySelectorAll('.fireball').forEach(fireball => {
         let posX = parseInt(fireball.style.left);
 
+        // Detect collision
+        bugElements.forEach(bug => {
+            if (detectCollision(bug, fireball)) {
+                bug.remove();
+                fireball.remove();
+            }
+        })
+
         if(posX > game.gameScreen.offsetWidth) {
-            fireball.remove()
+            fireball.remove();
         } else {
             fireball.style.left = posX + state.fireball.speed + 'px';
         }
@@ -80,3 +91,11 @@ function modifyWizardPosition(state, game) {
         wizard.posY = Math.min(wizard.posY + wizard.speed, game.gameScreen.offsetHeight - wizard.height);
     }
 }
+
+function detectCollision(objectA, objectB) {
+    let first = objectA.getBoundingClientRect();
+    let second = objectB.getBoundingClientRect();
+
+    let hasCollision = !(first.top > second.bottom || first.bottom < second.top || first.right < second.left || first.left > second.right);
+    return hasCollision;
+}   
